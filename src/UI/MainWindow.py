b0c4c3dace3c20bb4c0d44932aa4762e908b841b
Carrima Hewitt <chewitt1@ufl.edu>
2022-12-19 18:57:22 -0500

    Add Display

--- src/UI/MainWindow.py
+++ src/UI/MainWindow.py
@@ -3,6 +3,8 @@
     self.less.setEnabled(False)
     self.more.setEnabled(False)
     self.digital.setChecked(True)
+    self.green.setEnabled(False)
+    self.grey.setEnabled(False)
 """
 import sys
 from PyQt5.QtWidgets import QApplication
@@ -13,20 +15,38 @@ from version2 import Ui_MainWindow
 
 class MainWindow:
     def __init__(self):
+        self.go = False
         self.win = QMainWindow()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self.win)
 
-        self.ui.startTime.clicked.connect(lambda: self.setWindow(1))
-        self.ui.listTimers.currentIndexChanged.connect(self.setType)
+        self.ui.startTime.clicked.connect(self.startProgram)
+        self.ui.listTimers.currentIndexChanged.connect(self.setDisplay)
+        self.ui.less.clicked.connect(lambda: self.setGoal(1))
+        self.ui.more.clicked.connect(lambda: self.setGoal(0))
+
+        self.ui.digital.toggled.connect(self.setWindow)
+        self.ui.progress.toggled.connect(self.setWindow)
+        self.ui.checkpoint.toggled.connect(self.setWindow)
 
     def show(self):
         self.win.show()
 
-    def setWindow(self, num):  # 0 = home, 1 = digital, 2 = progress, 3 = done
-        self.ui.pages.setCurrentIndex(num)
+    def startProgram(self):
+        self.go = True
+        self.setWindow()
 
-    def setType(self):
+    def setWindow(self):  # 0 = home, 1 = digital, 2 = progress, 3 = done
+        if self.go:
+            if self.ui.digital.isChecked():
+                self.ui.pages.setCurrentIndex(1)
+            elif self.ui.progress.isChecked():
+                self.ui.pages.setCurrentIndex(2)
+            elif self.ui.checkpoint.isChecked():
+                self.ui.pages.setCurrentIndex(3)
+            self.setDisplay()
+
+    def setDisplay(self):
         if self.ui.digital.isChecked():
             self.setDigital()
         elif self.ui.progress.isChecked():
@@ -40,8 +60,8 @@ class MainWindow:
         if i == 0:
             self.ui.title_digital.setText("Choose Timer")
             self.ui.title_digital.update()
-            self.ui.timeToGo.setText("00:00")
-            self.ui.timeToGo.update()
+            self.ui.goal_digital.setText("00:00")
+            self.ui.goal_digital.update()
 
             self.ui.less.setEnabled(False)
             self.ui.more.setEnabled(False)
@@ -49,8 +69,8 @@ class MainWindow:
         elif i == 1:
             self.ui.title_digital.setText("Focus Timer")
             self.ui.title_digital.update()
-            self.ui.timeToGo.setText("15:00")
-            self.ui.timeToGo.update()
+            self.ui.goal_digital.setText("15:00")
+            self.ui.goal_digital.update()
 
             self.ui.less.setEnabled(True)
             self.ui.more.setEnabled(True)
@@ -60,8 +80,8 @@ class MainWindow:
         elif i == 2:
             self.ui.title_digital.setText("Pomodoro Timer")
             self.ui.title_digital.update()
-            self.ui.timeToGo.setText("25:00")
-            self.ui.timeToGo.update()
+            self.ui.goal_digital.setText("25:00")
+            self.ui.goal_digital.update()
 
             self.ui.less.setEnabled(False)
             self.ui.more.setEnabled(False)
@@ -71,8 +91,8 @@ class MainWindow:
         elif i == 3:
             self.ui.title_digital.setText("Class Timer")
             self.ui.title_digital.update()
-            self.ui.timeToGo.setText("50:00")
-            self.ui.timeToGo.update()
+            self.ui.goal_digital.setText("50:00")
+            self.ui.goal_digital.update()
 
             self.ui.less.setEnabled(True)
             self.ui.more.setEnabled(True)
@@ -85,8 +105,8 @@ class MainWindow:
         if i == 0:
             self.ui.title_progress.setText("Choose Timer")
             self.ui.title_progress.update()
-            self.ui.progress_goal.setText("0")
-            self.ui.progress_goal.update()
+            self.ui.goal_progress.setText("0")
+            self.ui.goal_progress.update()
 
             self.ui.less.setEnabled(False)
             self.ui.more.setEnabled(False)
@@ -94,8 +114,8 @@ class MainWindow:
         elif i == 1:
             self.ui.title_progress.setText("Focus Timer")
             self.ui.title_progress.update()
-            self.ui.progress_goal.setText("15")
-            self.ui.progress_goal.update()
+            self.ui.goal_progress.setText("15")
+            self.ui.goal_progress.update()
 
             self.ui.less.setEnabled(True)
             self.ui.more.setEnabled(True)
@@ -105,8 +125,8 @@ class MainWindow:
         elif i == 2:
             self.ui.title_progress.setText("Pomodoro Timer")
             self.ui.title_progress.update()
-            self.ui.progress_goal.setText("25")
-            self.ui.progress_goal.update()
+            self.ui.goal_progress.setText("25")
+            self.ui.goal_progress.update()
 
             self.ui.less.setEnabled(False)
             self.ui.more.setEnabled(False)
@@ -116,8 +136,8 @@ class MainWindow:
         elif i == 3:
             self.ui.title_progress.setText("Class Timer")
             self.ui.title_progress.update()
-            self.ui.progress_goal.setText("50")
-            self.ui.progress_goal.update()
+            self.ui.goal_progress.setText("50")
+            self.ui.goal_progress.update()
 
             self.ui.less.setEnabled(True)
             self.ui.more.setEnabled(True)
@@ -132,8 +152,8 @@ class MainWindow:
         if i == 0:
             self.ui.title_checkpoint.setText("Choose Timer")
             self.ui.title_checkpoint.update()
-            self.ui.checkpoint_goal.setText("0")
-            self.ui.checkpoint_goal.update()
+            self.ui.goal_checkpoint.setText("0")
+            self.ui.goal_checkpoint.update()
 
             self.ui.less.setEnabled(False)
             self.ui.more.setEnabled(False)
@@ -141,8 +161,8 @@ class MainWindow:
         elif i == 1:
             self.ui.title_checkpoint.setText("Focus Timer")
             self.ui.title_checkpoint.update()
-            self.ui.checkpoint_goal.setText("15")
-            self.ui.checkpoint_goal.update()
+            self.ui.goal_checkpoint.setText("15")
+            self.ui.goal_checkpoint.update()
 
             self.ui.less.setEnabled(True)
             self.ui.more.setEnabled(True)
@@ -152,8 +172,8 @@ class MainWindow:
         elif i == 2:
             self.ui.title_checkpoint.setText("Pomodoro Timer")
             self.ui.title_checkpoint.update()
-            self.ui.checkpoint_goal.setText("25")
-            self.ui.checkpoint_goal.update()
+            self.ui.goal_checkpoint.setText("25")
+            self.ui.goal_checkpoint.update()
 
             self.ui.less.setEnabled(False)
             self.ui.more.setEnabled(False)
@@ -163,8 +183,8 @@ class MainWindow:
         elif i == 3:
             self.ui.title_checkpoint.setText("Class Timer")
             self.ui.title_checkpoint.update()
-            self.ui.checkpoint_goal.setText("50")
-            self.ui.checkpoint_goal.update()
+            self.ui.goal_checkpoint.setText("50")
+            self.ui.goal_checkpoint.update()
 
             self.ui.less.setEnabled(True)
             self.ui.more.setEnabled(True)
@@ -174,6 +194,98 @@ class MainWindow:
         self.ui.point1.setProperty("value", 0)
         self.ui.point2.setProperty("value", 0)
         self.ui.point3.setProperty("value", 0)
+
+    def setGoal(self, c):
+        if self.ui.digital.isChecked():
+            self.setDigitalGoal(c)
+        elif self.ui.progress.isChecked():
+            self.setProgressGoal(c)
+        elif self.ui.checkpoint.isChecked():
+            self.setCheckpointGoal(c)
+
+    def setDigitalGoal(self, c):
+        i = self.ui.listTimers.currentIndex()
+        mins = int(self.ui.goal_digital.text().split(":")[0])
+
+        if c == 0:
+            if i == 1:
+                if mins < 240:
+                    self.ui.goal_digital.setText(str(mins + 15) + ":00")
+                    self.ui.goal_digital.update()
+
+            elif i == 3:
+                if mins < 360:
+                    mins += 1
+                    if (mins / 10) < 1:
+                        mins = "0" + str(mins)
+                    self.ui.goal_digital.setText(str(mins) + ":00")
+                    self.ui.goal_digital.update()
+
+        elif c == 1:
+            if i == 1:
+                if mins > 15:
+                    self.ui.goal_digital.setText(str(mins - 15) + ":00")
+                    self.ui.goal_digital.update()
+
+            elif i == 3:
+                if mins > 0:
+                    mins -= 1
+                    if (mins / 10) < 1:
+                        mins = "0" + str(mins)
+                    self.ui.goal_digital.setText(str(mins) + ":00")
+                    self.ui.goal_digital.update()
+
+    def setProgressGoal(self, c):
+        i = self.ui.listTimers.currentIndex()
+        mins = int(self.ui.goal_progress.text())
+
+        if c == 0:
+            if i == 1:
+                if mins < 240:
+                    self.ui.goal_progress.setText(str(mins + 15))
+                    self.ui.goal_progress.update()
+
+            elif i == 3:
+                if mins < 360:
+                    self.ui.goal_progress.setText(str(mins + 1))
+                    self.ui.goal_progress.update()
+
+        elif c == 1:
+            if i == 1:
+                if mins > 15:
+                    self.ui.goal_progress.setText(str(mins - 15))
+                    self.ui.goal_progress.update()
+
+            elif i == 3:
+                if mins > 0:
+                    self.ui.goal_progress.setText(str(mins - 1))
+                    self.ui.goal_progress.update()
+
+    def setCheckpointGoal(self, c):
+        i = self.ui.listTimers.currentIndex()
+        mins = int(self.ui.goal_checkpoint.text())
+
+        if c == 0:
+            if i == 1:
+                if mins < 240:
+                    self.ui.goal_checkpoint.setText(str(mins + 15))
+                    self.ui.goal_checkpoint.update()
+
+            elif i == 3:
+                if mins < 360:
+                    self.ui.goal_checkpoint.setText(str(mins + 1))
+                    self.ui.goal_checkpoint.update()
+
+        elif c == 1:
+            if i == 1:
+                if mins > 15:
+                    self.ui.goal_checkpoint.setText(str(mins - 15))
+                    self.ui.goal_checkpoint.update()
+
+            elif i == 3:
+                if mins > 0:
+                    self.ui.goal_checkpoint.setText(str(mins - 1))
+                    self.ui.goal_checkpoint.update()
 
 
 if __name__ == '__main__':
